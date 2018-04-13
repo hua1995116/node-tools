39e05e692cae3ef5b7c90ec2a6ed4d0e83e0d82e
hua1995116 <461249104@qq.com>
2018-04-13 11:35:10 +0800

    U: 增加选项

--- js-count.js
+++ js-count.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 const fs = require('fs');
 const path = require('path');
+var argv = require('yargs')
+    .usage('Usage: $0 -f [file] -i [directory]')
+    .argv;
 const root = process.cwd();
 const ignoreDir = ['node_modules'];
 const fileJsTypes = ['.js','.jsx'];
@@ -23,7 +26,7 @@ function getFileName(dir) {
               currentPath = path.join(dir, item),
               isFile = fs.statSync(currentPath).isFile(),
               isDir = fs.statSync(currentPath).isDirectory();
-
+        
         if(isFile) {
             if(fileJsTypes.indexOf(extname) > -1) {
                 readJsLine(currentPath);
@@ -53,7 +56,39 @@ function readCssLine(file) {
     filesCssLineNoBlank += lines.length;
 }
 
-getFileName(root);
+function main() {
+    if(argv.i){
+        const ignodeList = argv.i.indexOf(',') > -1 ? argv.i.split(','): [argv.i];
+        ignodeList.map((item) => {
+            const isDir = fs.statSync(item).isDirectory();
+            if(isDir) {
+                ignoreDir.push(item);
+            } else {
+                console.warn(`${item} is not a dir`);
+            }
+        })
+    }
+    if(argv.f) {
+        const fileList = argv.f.split(',') > -1 ? argv.f.split(','): [argv.f];
+        fileList.map((item) => {
+            const extname = path.extname(item);
+            const isFile = fs.statSync(item).isFile();
+            if(isFile) {
+                if(fileJsTypes.indexOf(extname) > -1) {
+                    readJsLine(item);
+                }
+                if(fileCssTypes.indexOf(extname) > -1) {
+                    readCssLine(item);
+                }
+            }
+        })
+    }
+    if(!argv.i && !argv.f){
+        getFileName(root);
+    }
+}
+
+main();
 
 console.log(`js/jsx lines：${filesJsLine} \n(rm empty lines)：${filesJsLineNoBlank}`);
 console.log(`css/sass/less/stylus lines：${filesCssLine} \n(rm empty lines)：${filesCssLineNoBlank}`);
